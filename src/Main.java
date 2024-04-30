9a414e53c728e86440770957a605a91a482686f1
devisrael <158238730+IsraelDev560@users.noreply.github.com>
2024-04-30 10:14:13 -0300

    fix: code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+public class MinhaClasse {
+    public static void main (String [] args) {
+        System.out.print("Olá, Mundo!");
     }
 }
